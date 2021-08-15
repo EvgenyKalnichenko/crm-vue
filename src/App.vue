7a08d4f0921c51daefca8518d01ff2a798564a4f
kalnichenko <kalnichenko.1996@mail.ru>
2021-08-15 15:55:43 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
