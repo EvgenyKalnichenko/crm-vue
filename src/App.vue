5085b68306f267441f7ec51ffc0c2e545cec26f2
kalnichenko <kalnichenko.1996@mail.ru>
2021-08-15 16:29:36 +0300

    Первичные настройки layout + router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <component :is="layout">
+            <router-view></router-view>
+        </component>
     </div>
 </template>
 
 <script>
+    import EmptyLayout from "./layouts/EmptyLayout";
+    import MainLayout from "./layouts/MainLayout";
 
-export default {
-  name: 'App',
-
-}
+    export default {
+        name: 'App',
+        computed: {
+            layout() {
+                console.log(this.$route.meta)
+                return (this.$route.meta.layout || 'empty')+'-layout'
+            }
+        },
+        components: {
+            MainLayout,
+            EmptyLayout
+        }
+    }
 </script>
 
 <style>
-@import "~materialize-css/dist/css/materialize.min.css";
-@import "assets/index.css";
+    @import "~materialize-css/dist/css/materialize.min.css";
+    @import "assets/index.css";
 </style>
--- src/router.js
+++ src/router.js
@@ -3,7 +3,60 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        {
+            path: '/login',
+            name: 'login',
+            meta: {layout: 'empty'},
+            component: () => import('./views/Login')
+        },
+        {
+            path: '/register',
+            name: 'register',
+            meta: {layout: 'empty'},
+            component: () => import('./views/Register')
+        },
+        {
+            path: '/categories',
+            name: 'categories',
+            meta: {layout: 'main'},
+            component: () => import('./views/Categories')
+        },
+        {
+            path: '/detail-record',
+            name: 'detail-record',
+            meta: {layout: 'main'},
+            component: () => import('./views/Detail-record')
+        },
+        {
+            path: '/history',
+            name: 'history',
+            meta: {layout: 'main'},
+            component: () => import('./views/History')
+        },
+        {
+            path: '/',
+            name: 'home',
+            meta: {layout: 'main'},
+            component: () => import('./views/Home')
+        },
+        {
+            path: '/planning',
+            name: 'planning',
+            meta: {layout: 'main'},
+            component: () => import('./views/Planning')
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            meta: {layout: 'main'},
+            component: () => import('./views/Profile')
+        },
+        {
+            path: '/record',
+            name: 'record',
+            meta: {layout: 'main'},
+            component: () => import('./views/Record')
+        }
     ],
 })
 
