e19dd39f9a160458a59d5244df0e5edaaf7d8b08
kalnichenko <kalnichenko.1996@mail.ru>
2021-08-16 17:55:37 +0300

    валидация на странице login с помощью vuelidate (5 урок)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,33 @@
 <template>
-    <form class="card auth-card">
+    <form class="card auth-card" @submit.prevent="submitHandler">
         <div class="card-content">
             <span class="card-title">Домашняя бухгалтерия</span>
             <div class="input-field">
                 <input
                         id="email"
                         type="text"
-                        class="validate"
-                >
+                        v-model.trim="email"
+                        :class="{invalid: (this.v$.email.$dirty && this.v$.email.$error) }"
+                   >
                 <label for="email">Email</label>
-                <small class="helper-text invalid">Email</small>
+                <small class="helper-text invalid" v-if="this.v$.email.$dirty && this.v$.email.$error">
+                    Некоректно введен Email
+                </small>
             </div>
             <div class="input-field">
                 <input
                         id="password"
                         type="password"
-                        class="validate"
+                        v-model.trim="password"
+                        :class="{invalid: (this.v$.password.$dirty && this.v$.password.$invalid) }"
                 >
                 <label for="password">Пароль</label>
-                <small class="helper-text invalid">Password</small>
+                <small class="helper-text invalid" v-if="this.v$.password.$dirty && this.v$.password.$error">
+                    Пароль не должен быть меньше 6 символов
+                </small>
             </div>
         </div>
+<!--        <pre>{{this.v$.password}}</pre>-->
         <div class="card-action">
             <div>
                 <button
@@ -34,15 +41,51 @@
 
             <p class="center">
                 Нет аккаунта?
-                <a href="/">Зарегистрироваться</a>
+                <router-link
+                    to="/register"
+                >
+                    Зарегистрироваться
+                </router-link>
             </p>
         </div>
     </form>
 </template>
 
 <script>
+    import { email, required, minLength } from '@vuelidate/validators'
+    import { useVuelidate } from '@vuelidate/core'
+
     export default {
-        name: "Login"
+        name: "Login",
+        data(){
+            return{
+                password: '',
+                email: '',
+            }
+        },
+        setup: () => ({ v$: useVuelidate() }),
+        validations: {
+            email: {
+                required,
+                email,
+            },
+            password: {
+                required,
+                minLength: minLength(6)
+            }
+        },
+        methods:{
+            submitHandler(){
+                if(this.v$.$invalid){
+                    this.v$.$touch()
+                    return;
+                }
+                this.$router.push('/');
+            }
+        },
+        mounted() {
+
+        }
     }
 </script>
 
